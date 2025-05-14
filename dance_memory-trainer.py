64873d732a6b239232fb1ab91e213ed6af8e51e1
sydneyriccio <sydney_riccio@brown.edu>
2025-05-13 23:11:58 -0400

    Victoria and Sydney added Steps 3, 4, and 5 over 10 hours (had to research and debug)

--- dance_memory-trainer.py
+++ dance_memory-trainer.py
@@ -6,7 +6,7 @@
 import random; # helps to shuffle random dance moves in each round
 
 # matplotlib is a library we looked up and learned to use visualizing performance data
-import matplotlib.pyplot as plt; % used to create a graph of the player's performance after the game
+import matplotlib.pyplot as plt; # used to create a graph of the player's performance after the game
 Dance_Moves = [
     "spin - turn to your right", 
     "pop - sharp chest movement", 
@@ -30,3 +30,31 @@ class DanceMemoryGame: # Class used to keep game logic organized and scalable
         self.round = 1
         self.performance = []
         self.detailed_results = [] # Analyzing memory patterns 
+
+# Step 3: Show the sequence to the player
+def show_sequence(self):
+    print(f"\nRound {self.round}:")
+    for move in self.sequence:
+        print(move)
+    input("\nPress Enter when you are ready to repeat the sequence...")
+
+# Step 4: Get player or simulated input 
+def get_player_input(self): 
+    if self.mode == "simulate": #Simulates a player with a fixed memory limit: wrote this to model cognitive overload and working memory limits in psychology. 
+        if len(self.sequence) <= self.memory_limit:
+            return self.sequence.copy()
+        else:
+            # We intentionally add an incorrect final move to simulate a recall failure
+            return self.sequence[:self.memory_limit -1] + ["wrong"]
+    else:
+        print("'\nType the sequence of moves (separated by spaces!):")
+        return input (">").strip().lower().split()
+    
+# Step 5: Play a single round
+def play_round(self):
+    # Adds a new random move each round, making the sequence longer and harder to remember
+    new_move = random.choice(DANCE_MOVES)
+    self.sequence.append(new_move)
+
+    self.show_sequence()
+    player_input = self.get_player_input ()
